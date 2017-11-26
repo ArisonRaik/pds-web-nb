320944681a18fdb4c6e0e25dfb664081dbd81c36
Victor Wagner <victorwdr@gmail.com>
2017-11-26 13:15:08 -0300

    changes  has been made

--- registration-database/docker-entrypoint-initdb.d/initialize_db.sql
+++
@@ -1,8 +0,0 @@
-USE `dockercon2035`;
-
-CREATE TABLE `mensagem` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `idPaciente` bigint(20) NOT NULL,
-  `idAlerta` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
